c7a2f8c04438996ec42fa05306658129eca18f7b
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2024-09-15 23:07:42 +0800

    setup migration & model

---
+++ database/migrations/2024_09_15_215232_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->unsignedBigInteger('price');
+            $table->text('about');
+            $table->text('caption');
+            $table->boolean('is_recommend')->default(false);
+            $table->unsignedBigInteger('views');
+            $table->string('thumbnail');
+
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
