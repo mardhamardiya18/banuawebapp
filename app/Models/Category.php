3edb2cc5b15799a45f6ce928189e3bdc48f75cc2
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2024-09-15 23:15:36 +0800

    setup orm relationship

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
@@ -13,4 +14,15 @@ class Category extends Model
         'name',
         'slug'
     ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
@@ -20,4 +21,25 @@ class Product extends Model
         'is_recommend',
         'category_id'
     ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function getPriceAttribute($value)
+    {
+        return 'Rp ' . number_format($value, 0, ',', '.');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function galleries()
+    {
+        return $this->hasMany(Gallery::class);
+    }
 }
